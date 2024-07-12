c8c300c8f5e7672efbe83b960bea41120df4dcf7
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-07-12 08:29:38 -0400

    [feat]: Object Manipulation

---
+++ object_manipulation.js
@@ -0,0 +1,14 @@
+const books = [
+  { title: '1984', author: 'George Orwell', year: 1949 },
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  { title: 'Pride and Prejudice', author: 'Jane Austen', year: 1813 },
+];
+
+const extractTitles = (books) => {
+  return books.map((book) => book.title);
+};
+
+const bookTitles = extractTitles(books);
+
+console.log(bookTitles);
